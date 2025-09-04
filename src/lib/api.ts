f173137513e3d2dd359dc7a00d7a344b9e1b1ecc
ALittlePatate <maxime@patate.dev>
2025-09-04 16:15:39 +0200

    fix: api port

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:80/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:24680/api';
 
 export const apiClient = {
     get: async (endpoint: string) => {
