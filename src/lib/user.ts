126d552a112a799d1fdd228bbd746226f3046ece
Yuuko <tilio.huart@epitech.eu>
2025-09-05 09:22:25 +0200

    fix: update token handling

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -3,16 +3,9 @@
 import { apiClient } from "@/lib/api";
 
 export async function getUserInformation() {
-    const token = localStorage.getItem("token");
   try {
     const response = await apiClient.get(
       "/user/me",
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
-        },
-      }
     );
 
     console.log("Information", response);
