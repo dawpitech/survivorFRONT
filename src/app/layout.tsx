1fbe2c46a0857aee9b856d22e87d052c01eec5cf
Valiance <lucas.ludovic@epitech.eu>
2025-09-11 10:48:29 +0200

    fix: footer at the bottom of the page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,39 +4,41 @@ import "./globals.css";
 import NavBar from "@/components/layout/NavBar";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "JEB Incubator",
-  description: "JEB Incubator",
+    title: "JEB Incubator",
+    description: "JEB Incubator",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <header>
-          <NavBar />
-        </header>
-        {children}
-        <footer className="mt-16 pt-8 pb-6 border-t border-gray-200 bg-gray-50">
-          <p className="text-center text-gray-600">© 2025 JEB Incubator</p>
-        </footer>{" "}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
+            >
+                <header>
+                    <NavBar />
+                </header>
+                <section className="flex-grow">
+                    {children}
+                </section>
+                <footer className="mt-16 pt-8 pb-6 border-t border-gray-200 bg-gray-50 mt-auto">
+                    <p className="text-center text-gray-600">© 2025 JEB Incubator</p>
+                </footer>{" "}
+            </body>
+        </html>
+    );
 }
 
